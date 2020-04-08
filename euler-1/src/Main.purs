643141d987cc12b169f85504433dc97f02ab7c96
Jakie W. Rice <jakie.rice@corva.ai>
2020-04-08 14:24:21 -0500

    feat: add Eular module and log answer from eular exercise Main

--- euler-1/src/Main.purs
+++ euler-1/src/Main.purs
@@ -2,9 +2,11 @@ module Main where
 
 import Prelude
 
+import Euler (answer)
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
   log "🍝"
+  log ("the answer is " <> show answer)
